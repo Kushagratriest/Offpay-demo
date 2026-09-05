c8ebba3bfb1c78c5113e75b784d9195642f817bd
Kushagra <kushagrabhushan23@gmail.com>
2026-09-06 02:44:40 +0530

    Update sw.js

--- offpay/public/sw.js
+++ offpay/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "offpay-cache-v2";
+const CACHE_NAME = "offpay-cache-v3";
 const OFFLINE_URLS = ["/", "/index.html", "/manifest.json", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", (event) => {
